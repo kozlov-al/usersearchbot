7a35ced2154ab3b509a85f3c531a7259364b0358
jgjg228 <Sasha8786@yandex.ru>
2020-04-29 18:10:33 +0500

    (feat) keyboard

--- app/Telegram/TestCommand.php
+++ app/Telegram/TestCommand.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Request;
 use Telegram\Bot\Actions;
 use Telegram\Bot\Commands\Command;
 use Telegram\Bot\Laravel\Facades\Telegram;
+use Telegram\Bot\Keyboard\Keyboard;
 
 /**
  * Class HelpCommand.
@@ -30,37 +31,60 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        $text ='';
+
+        $telegram = $this->telegram;
+        /**
+         * profile info
+         */
+        $text = '';
         $this->replyWithChatAction(['action' => Actions::TYPING]);
         $update = $this->getUpdate();
         $name = $update->getMessage()->from->firstName;
         $f_name = $update->getMessage()->from->lastName;
         $user = User::find(1);
         $email = 'Почта пользователя в laravel: ' . $user->email;
-        $text = $name . ' ' . $f_name. '  '.PHP_EOL . $email;
-        $this->replyWithMessage(compact('text'));
+        $text = $name . ' ' . $f_name . '  ' . PHP_EOL . $email;
+        /**
+         * users
+         */
+        $users = User::all();
+        $users_info = 'Список пользователей Laravel: ' . PHP_EOL;
+        foreach ($users as $user) {
+            $users_info .= $user->name . ' ' . $user->email . PHP_EOL;
+        }
+        /**
+         * keyboard
+         */
+        $keyboard = Keyboard::make()
+            ->setResizeKeyboard(true)
+            ->setOneTimeKeyboard(true)
+            ->row(
+                Keyboard::inlineButton(['text' => 'Список пользователей на сайте', 'callback_data' => '1']),
+                Keyboard::inlineButton(['text' => 'Ясно', 'callback_data' => '/butt2'])
+            );
+        $this->replyWithMessage(['text' => $text, 'reply_markup' => $keyboard]);
 
 
-//        $keyboard = [
-//            ['7', '8', '9'],
-//            ['4', '5', '6'],
-//            ['1', '2', '3'],
-//            ['0']
-//        ];
-//
-//        $reply_markup = $this->telegram->replyKeyboardMarkup([
-//            'keyboard' => $keyboard,
-//            'resize_keyboard' => true,
-//            'one_time_keyboard' => true
-//        ]);
-//
-//        $this->telegram->sendMessage([
-//            'chat_id' => $this->telegram->getMe()->id,
-//            'text' => 'Hello World',
-//            'reply_markup' => $reply_markup
-//        ]);
+        $arUpdates = $telegram->getUpdates();
 
 
+            foreach ($arUpdates['result'] as $arResult) {
+                if (array_key_exists('callback_query',$arResult)) {
+
+                    $userId = $arResult['callback_query']['from']['id'];
+
+                    if ($arResult['callback_query']['data'] == 1) {
+                        $telegram->sendMessage([$userId, 'Its ok!']);
+                    } else {
+                        $telegram->sendMessage([$userId, 'Its not ok!']);
+                    }
+                }
+            }
+        }
+
+
+        //$this->telegram->sendMessage(['text' => $users_info])
+        //$this->replyWithMessage(['text' => $users_info])
 
 
     }
